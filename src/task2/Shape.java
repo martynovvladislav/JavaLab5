722e04267eab54c39bc967b8f4c0f26940bc1a9f
Vlad <martynovq03@gmail.com>
2023-09-25 16:58:04 +0300

    Initial commit

---
+++ src/task2/Shape.java
@@ -0,0 +1,16 @@
+package task2;
+import java.awt.*;
+
+public abstract class Shape {
+    public Color color;
+    public double xpos;
+    public double ypos;
+
+    public Shape(Color color, double xpos, double ypos) {
+        this.color = color;
+        this.xpos = xpos;
+        this.ypos = ypos;
+    }
+
+    public abstract void draw(Graphics g);
+}
